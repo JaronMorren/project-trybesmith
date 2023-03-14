a53132a42cbc496c56e711e92e0ae5141bf7b2bb
jaron morren <jaronmorren@gmail.com>
2023-03-14 17:55:40 -0300

    added productsRouter

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -4,7 +4,6 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   password: process.env.MYSQL_PASSWORD,
   user: process.env.MYSQL_USER,
-  database: process.env.MYSQL_DATABASE,
 });
 
 export default connection;
