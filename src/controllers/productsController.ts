961e29c274a30a457890fd5e10a845b93dc485a9
jaron morren <jaronmorren@gmail.com>
2023-03-16 15:11:21 -0300

    Added listProducts to controller layer

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -2,6 +2,15 @@ import { Request, Response } from 'express';
 import productsService from '../services/productsService';
 import HttpException from './httpException';
 
+const listProducts = async (_request: Request, response: Response) => {
+  try {
+    const list = await productsService.listProducts();
+    return response.status(200).json(list);
+  } catch (error) {
+    const errorMessage = error as Error;
+    throw new HttpException(500, errorMessage.message);
+  }
+};
 const createProduct = async (request: Request, response: Response) => {
   try {
     const newProduct = await productsService.createProduct(request.body);
@@ -12,4 +21,4 @@ const createProduct = async (request: Request, response: Response) => {
   }
 };
 
-export default { createProduct };
+export default { createProduct, listProducts };
