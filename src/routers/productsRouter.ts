cc0cd2881892ce946d61ddcbbd3af0141faf635e
jaron morren <jaronmorren@gmail.com>
2023-03-15 22:07:33 -0300

    Req1: Created endpoint to register new products

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import productsController from '../controllers/productsController';
 
 const productsRouter = Router();
+
+productsRouter.post('/', productsController.createProduct);
 
 export default productsRouter;
