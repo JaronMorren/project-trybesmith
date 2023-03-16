a566b1aad648a762a7df5868215e4e04967bc9c4
jaron morren <jaronmorren@gmail.com>
2023-03-16 14:58:54 -0300

    Added listProducts to service layer

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,9 +1,14 @@
-import { NewProduct, CreatedProduct } from '../interfaces/productsInterface';
+import { NewProduct, CreatedProduct, ListProducts } from '../interfaces/productsInterface';
 import productsModel from '../models/productsModel';
+
+const listProducts = async (): Promise<ListProducts[]> => {
+  const list = await productsModel.listProducts();
+  return list;
+};
 
 const createProduct = async (product: NewProduct): Promise<CreatedProduct> => {
   const newProduct = await productsModel.createProduct(product);
   return newProduct;
 };
 
-export default { createProduct };
+export default { createProduct, listProducts };
