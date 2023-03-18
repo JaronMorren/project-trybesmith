1139cfb9eede19b630ee33a6afa96770482ec8af
jaron morren <jaronmorren@gmail.com>
2023-03-18 18:11:27 -0300

    fixed error with returning token

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -4,8 +4,8 @@ import HttpException from './httpException';
 
 const createUser = async (request: Request, response: Response) => {
   try {
-    const newUser = await usersService.createUser(request.body);
-    return response.status(201).json({ newUser });
+    const token = await usersService.createUser(request.body);
+    return response.status(201).json({ token });
   } catch (error) {
     const errorMessage = error as Error;
     throw new HttpException(500, errorMessage.message);
