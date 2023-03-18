284969def474b1a8b55070b4b9f3622a5618b28f
jaron morren <jaronmorren@gmail.com>
2023-03-18 18:52:59 -0300

    Created ordersModel

---
+++ src/models/ordersModel.ts
@@ -0,0 +1,17 @@
+import { RowDataPacket } from 'mysql2';
+import { Order } from '../interfaces/ordersinterface';
+import connection from './connection';
+
+const getOrders = async ():Promise<Order[]> => {
+  const [orders] = await connection.execute<RowDataPacket[] & Order[]>(
+    `SELECT orders.id, orders.user_id AS userId, JSON_ARRAYAGG(products.id) AS productsIds
+    FROM Trybesmith.orders
+    INNER JOIN Trybesmith.products
+    ON orders.id = products.order_id GROUP BY orders.id`,
+  );
+
+  return orders;
+};
+
+export default { getOrders }; 
+// https://dev.mysql.com/doc/refman/5.7/en/aggregate-functions.html#function_json-arrayagg
